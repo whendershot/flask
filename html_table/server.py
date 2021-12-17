211c6baf8e74199194f1c8d126ea6a034ee14118
William Hendershot <whendershot1@gmail.com>
2021-12-17 15:27:49 +0900

    Initial implementation

---
+++ html_table/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__.split('.')[0])
+
+@app.route('/')
+def render_lists():
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    return render_template('./lists.html', alist = users)
+
+@app.route('/', defaults={'u_path' : ''})
+@app.route('/<path:u_path>')
+def catch_all(u_path):
+    print(repr(u_path))
+    return f'Sorry! No response. Try again. But how did you fall into here?<br>{u_path}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
