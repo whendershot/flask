ab84cf9222d837c88e48fb043a10fba477f18cb7
William Hendershot <whendershot1@gmail.com>
2021-12-20 12:22:08 +0900

    Initial implementation

--- dojo_fruit_store/server.py
+++ dojo_fruit_store/server.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__.split('.')[0])  
+from flask import Flask, render_template, request, redirect, session
+from datetime import datetime, timezone
+
+app = Flask(__name__.split('.')[0])
+app.secret_key = 'Insert movie reference here...'
 
 @app.route('/')         
 def index():
@@ -8,7 +11,20 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    form = request.form
+    num_fruits = int(form['apple']) + int(form['strawberry']) + int(form['raspberry']) + int(form['blackberry'])
+    session['last_checkout'] = request.form
+    session['num_fruits'] = num_fruits
+    session['submission_timestamp'] = datetime.now(timezone.utc)
+    print(f'Charging {form["first_name"] + " " + form["last_name"]} for {num_fruits} fruits.')
+    #handle the purchase request
+    return redirect("/checked_out")
+
+@app.route('/checked_out')
+def post_checkout():
+    print(f"In the post checkout phase: {session['last_checkout']}")
+    return render_template("checkout.html", cart_info=session['last_checkout'])
+
 
 @app.route('/fruits')         
 def fruits():
